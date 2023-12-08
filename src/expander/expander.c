04cbcf50699d9bd14d4a90a4ee7934bd86acd744
Kai Klockow <kklockow@1-A-2.42heilbronn.de>
2023-12-08 16:07:45 +0100

    expander now able to replace with var

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,13 +6,17 @@
 /*   By: kklockow <kklockow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 12:54:21 by kklockow          #+#    #+#             */
-/*   Updated: 2023/12/08 13:24:40 by kklockow         ###   ########.fr       */
+/*   Updated: 2023/12/08 15:59:16 by kklockow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	expander(t_shell *shell, t_cmd *c_table);
+int		expander(t_shell *shell, t_cmd *c_table);
+int		get_sign_location(char *str);
+char	*get_variable_to_expand(char *str, int sign_location);
+char	*search_for_var(char *var, char **envp);
+void	update_cmd(t_cmd *c_table, char *var, int start, char *name);
 
 int	main(int ac, char **av, char **envp)
 {
@@ -28,7 +32,7 @@ int	main(int ac, char **av, char **envp)
 	ac = 0;
 	av = NULL;
 	c_table = malloc(sizeof (t_cmd));
-	c_table->cmd = "$new";
+	c_table->cmd = "sahne $USER sahne $USER";
 	c_table->infile = NULL;
 	c_table->outfile = NULL;
 	c_table->read_pipe = 0;
@@ -37,12 +41,123 @@ int	main(int ac, char **av, char **envp)
 	c_table->next = NULL;
 	shell = malloc(sizeof (t_shell *));
 	shell->envp = init_env(envp);
+	printf("before: %s\n", c_table->cmd);
 	expander(shell, c_table);
-	printf("%s\n", c_table->cmd);
+	printf("after: %s\n", c_table->cmd);
 	return (0);
 }
 
 int	expander(t_shell *shell, t_cmd *c_table)
 {
+	int		sign_location;
+	char	*var;
+	char	*var_content;
+
+	sign_location = get_sign_location(c_table->cmd);
+	if (sign_location == -1)
+		return (0);
+	var = get_variable_to_expand(c_table->cmd, sign_location);
+	var_content = search_for_var(var, shell->envp);
+	printf("%i\n%s\n%s\n", sign_location, var, var_content);
+	update_cmd(c_table, var_content, sign_location, var);
+	free(var);
+	free(var_content);
+	expander(shell, c_table);
 	return (0);
+}
+
+int	get_sign_location(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != '$')
+		i++;
+	if (!(str[i]))
+		return (-1);
+	return (i);
+}
+
+char	*get_variable_to_expand(char *str, int sign_location)
+{
+	int		i;
+	int		len;
+	char	*var_str;
+	int		var_i;
+
+	i = sign_location + 1;
+	len = 0;
+	while (str[i] && str[i] != ' ')
+	{
+		len++;
+		i++;
+	}
+	var_str = malloc(sizeof (char) * (len + 1));
+	i = sign_location + 1;
+	var_i = 0;
+	while (str[i] && str[i] != ' ')
+	{
+		var_str[var_i] = str[i];
+		i++;
+		var_i++;
+	}
+	var_str[var_i] = '\0';
+	return (var_str);
+}
+
+char	*search_for_var(char *var, char **envp)
+{
+	int		i;
+	char	*var_content;
+
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		if (ft_strncmp(var, envp[i], ft_strlen(var)) == 0)
+		{
+			var_content = ft_strdup(envp[i] + 1 + ft_strlen(var));
+			return (var_content);
+		}
+		i++;
+	}
+	var_content = malloc(sizeof (char) * 1);
+	var_content[0] = '\0';
+	return (var_content);
+}
+
+//ADD FREE C_TABLE->CMD WHEN MERGED!! IT IS ALLOCATED MEMORY FOR GODS SAKE!
+
+void	update_cmd(t_cmd *c_table, char *var, int start, char *name)
+{
+	char	*new;
+	int		i_old;
+	int		i_new;
+	int		i_var;
+
+	new = malloc(sizeof (char) * (ft_strlen(var) + ft_strlen(c_table->cmd) - ft_strlen(name) - 1));
+	i_new = 0;
+	i_old = 0;
+	while (i_old < start)
+	{
+		new[i_new] = c_table->cmd[i_old];
+		i_new++;
+		i_old++;
+	}
+	i_var = 0;
+	while (var[i_var] != '\0')
+	{
+		new[i_new] = var[i_var];
+		i_new++;
+		i_var++;
+	}
+	i_old += ft_strlen(name) + 1;
+	while (c_table->cmd[i_old] != '\0')
+	{
+		new[i_new] = c_table->cmd[i_old];
+		i_new++;
+		i_old++;
+	}
+	// free(c_table->cmd);
+	new[i_new] = '\0';
+	c_table->cmd = new;
 }
