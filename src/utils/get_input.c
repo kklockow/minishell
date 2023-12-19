c7fbe0d252fa048f0d0482d7c719ff317e67af7f
Fritz Gäbler <58051701+fritz-gabler@users.noreply.github.com>
2023-12-19 16:12:05 +0100

    add get_input structure to make tester possible

--- src/utils/get_input.c
+++ src/utils/get_input.c
@@ -6,12 +6,52 @@
 /*   By: kklockow <kklockow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 11:07:21 by fgabler           #+#    #+#             */
-/*   Updated: 2023/12/18 14:37:46 by fgabler          ###   ########.fr       */
+/*   Updated: 2023/12/19 16:10:19 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	get_input(t_coordinate *coordinate)
+{
+	if (isatty(fileno(stdin)))
+	{
+		ft_bzero(&coordinate->process, sizeof(t_process));
+		coordinate->input = readline("minishell> " );
+		if (coordinate->input == NULL)
+		{
+			coordinate->run_loop = false;
+			ft_bzero(&coordinate->process, sizeof(t_process));
+			ft_putstr_fd("minishell> exit\n", 2);
+		}
+		else
+		{
+			add_history(coordinate->input);
+			coordinate->process.time_to_setup = true;
+		}
+	}
+	else
+	{
+		ft_bzero(&coordinate->process, sizeof(t_process));
+		char *line;
+		line = get_next_line(fileno(stdin));
+		if (line == NULL)
+		{
+		coordinate->run_loop = false;
+		coordinate->process.time_to_lex = false;
+		ft_bzero(&coordinate->process, sizeof(t_process));
+		return ;
+		}
+		else
+		{
+		//add_history(coordinate->input);
+		coordinate->process.time_to_setup = true;
+		}
+		coordinate->input = ft_strtrim(line, "\n");
+	}
+}
+
+/*
 void	get_input(t_coordinate *coordinate)
 {
 	ft_bzero(&coordinate->process, sizeof(t_process));
@@ -26,5 +66,5 @@ void	get_input(t_coordinate *coordinate)
 	{
 		add_history(coordinate->input);
 		coordinate->process.time_to_setup = true;
-	}
 }
+*/
