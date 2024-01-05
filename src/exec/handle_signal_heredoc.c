22ffed9c1a2e2bf8e7138b82037f640c83b3a023
fritz-gabler <fritzlmgaebler@gmail.com>
2024-01-05 13:40:08 +0100

    add free functionality in heredoc when using ctrl c

---
+++ src/exec/handle_signal_heredoc.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_signal_heredoc.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgabler <mail@student.42heilbronn.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 13:05:44 by fgabler           #+#    #+#             */
+/*   Updated: 2024/01/05 13:29:55 by fgabler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static	void	heredoc_handler(int signum, siginfo_t *info, void *context);
+
+void	handle_signal_heredoc()
+{
+	struct sigaction	heredoc_signal;
+
+	heredoc_signal.sa_sigaction = heredoc_handler;
+	sigaction(SIGINT, &heredoc_signal, NULL);
+}
+
+static	void	heredoc_handler(int signum, siginfo_t *info, void *context)
+{
+	t_cmd	**command;
+	t_shell	**shell;
+	(void) info;
+	(void) context;
+
+	if (signum == SIGINT)
+	{
+		command = get_command_struct();
+		shell = get_shell_struct();
+		clean_exit(1, *shell, *command);
+	}
+}
