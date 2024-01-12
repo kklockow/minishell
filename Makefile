ef6007eee1410f6d68c3837b99363d2ca0cd49ad
Fritz Gäbler <58051701+fritz-gabler@users.noreply.github.com>
2024-01-12 13:16:01 +0100

    add new sort

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ NAME = minishell
 ###############################################################################
 
 CC				=		cc
-CFLAGS			=		-Wall -Werror -Wextra #-fsanitize=address -g
+CFLAGS			=		-Wall -Werror -Wextra
 EXTRA_FLAGS		=		-lreadline
 HEADERS			=		-I./includes -I./libs/include
 LIBS			=		./libs
@@ -16,11 +16,11 @@ LIBS_NAME		=		./libs/libs.a
 ###############################################################################
 ###############################################################################
 
-VPATH		:=		src src/lexer src/command_table src/utils src/init		\
-					src/builtins src/exec src/expander src/signals			\
+VPATH		:=		src src/lexer src/command_table src/handle_process		\
+					src/init src/builtins src/exec src/expander src/signals	\
 					src/free_structs src/setup_structs
 
-SRC_MAIN	:=		main.c
+SRC_MAIN	:=		minishell.c
 SRC_LEXER	:=		lexer.c find_quote_pair.c add_token_node.c				\
 					go_to_last_lexer_node.c double_greater_and_smaler.c		\
 					get_single_tokens.c tag_word.c skip_whitespace.c		\
@@ -30,14 +30,13 @@ SRC_CMD_TAB	:=		command_table.c syntax_error_print.c syntax_check.c		\
 					is_redirect.c fill_command_struct.c						\
 					command_node_add_back.c add_space_check.c				\
 					heredoc_argument.c repeat_set_data_next_save.c
-SRC_UTILS	:=		get_input.c stop_process.c								\
-					input_check.c set_error_code.c stop_loop.c				\
-					if_null_stop_process.c get_structs.c
-SRC_INIT	:=		init.c init_env.c
+SRC_PROCESS	:=		stop_process.c set_error_code.c stop_loop.c				\
+					if_null_stop_process.c
+SRC_INIT	:=		init.c init_env.c input_check.c get_input.c
 SRC_BUIN	:=		builtins_cd.c builtins.c builtins_utils_00.c			\
 					builtins_exit.c builtins_export.c builtins_utils_01.c
 SRC_EXEC	:=		exec_utils00.c executor.c heredoc_handling.c redirect.c \
-					exec_utils01.c
+					exec_utils01.c get_structs.c
 SRC_EXPAND	:=		expander.c update_cmd.c expand_to_home.c expander_utils.c
 SRC_SIGNAL	:=		catch_signals.c command_c.c command_quit.c				\
 					hide_ctrl_chars.c handle_signal_in_child.c
@@ -45,7 +44,7 @@ SRC_FREE	:=		free_lexer_struct.c parser_free.c						\
 					save_free.c shell_struct_free.c free_command_struct.c
 SRC_SETUP	:=		setup_structs.c first_setup.c
 
-SOURCE 		:=		$(SRC_MAIN) $(SRC_LEXER) $(SRC_CMD_TAB) $(SRC_UTILS)	\
+SOURCE 		:=		$(SRC_MAIN) $(SRC_LEXER) $(SRC_CMD_TAB) $(SRC_PROCESS)	\
 					$(SRC_INIT) $(SRC_BUIN) $(SRC_EXEC) $(SRC_EXPAND)		\
 					$(SRC_SIGNAL) $(SRC_FREE) $(SRC_SETUP)
 
@@ -75,9 +74,9 @@ $(OBJ_DIR)/%.o: %.c
 	@printf $(UP)$(CUT)
 
 $(LIBS_NAME):
+	@git submodule update --remote --init -q
 	@$(MAKE) -C $(LIBS) -B
 
-#@git submodule update --remote --init -q
 ###############################################################################
 ###############################################################################
 
--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: kklockow <kklockow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 10:35:47 by kklockow          #+#    #+#             */
-/*   Updated: 2024/01/11 17:58:24 by fgabler          ###   ########.fr       */
+/*   Updated: 2024/01/12 13:12:53 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,62 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 //INIT
+void	get_input(t_coordinate *coordinate);
+void	input_check(int ac, char **av, t_coordinate *coordinate);
 char	**init_env(char **envp);
 int		ft_env_len(char **envp);
 char	*ft_strdup_init(const char *s1);
 
+//SETUP
+void	first_setup(t_coordinate *coordinate, char **envp);
+void	setup_structs(t_coordinate *coordinate);
+
 //LEXER
+int		lexing(t_lexer *lexer, t_process *process);
 void	move_to_next_token(int	*position, char *input);
+void	move_to_next_token(int	*position, char *input);
+int		find_quote_pair(t_lexer *lexer);
+int		add_token_node(t_lexer *lexer);
+int		get_single_tokens(t_lexer *lexer, char token);
+int		double_greater_and_smaler(t_lexer *lexer);
+int		tag_word(t_lexer *lexer);
+int		time_to_lex(t_lexer *lexer);
+void	skip_whitespace(t_lexer *lexer);
+void	free_struct_and_stop_lexing(t_lexer **lexer);
+t_data	*go_to_last_lexer_node(t_data *lexer);
+
+
+
+//COMMAND TABLE
+int		add_space_check(t_data *data);
+int		is_redirect(t_data *data);
+int		command_table(t_parser *parser, t_process *process);
+void	command_node_add_back(t_cmd **command, t_parser *parser);
+void	fill_command_struct(t_parser *parser);
+void	pipe_roules_check(t_parser *parser);
+void	redirect_roules_check(t_parser *parser);
+void	syntax_check(t_parser *parser);
+void	setup_parser_struct(t_parser *parser, t_cmd *command, t_lexer *lexer);
+void	syntax_error_print(t_data *data);
+void	heredoc_argument(t_data *data, t_cmd *command);
+void	repeat_set_data_next_save(t_data **data);
+
+//EXPANDER
+int		expander(t_shell *shell, t_data *s);
+void	expand(t_parser *s);
+int		expand_to_home(t_shell *shell, t_data *s);
+int		get_sign_location(char *str);
+char	*get_variable_to_expand(char *str, int sign_location);
+char	*search_for_var(char *var, char **envp, t_shell *shell);
+char	*split_variable(char *str);
+void	handle_non_sign(t_data *s);
+void	free_and_do_again(char *var, char *vc, t_shell *shell, t_data *s);
+void	update_cmd(t_data *s, char *var, int start, char *name);
+int		get_len_export(char *str);
 
 //EXECUTOR
+t_cmd	**get_command_struct(void);
+t_shell	**get_shell_struct(void);
 int		executor_main(t_cmd *command, t_shell *shell);
 int		redirect(t_cmd *c_table, int *pipefd, t_shell *shell);
 void	check_envp(char **envp);
@@ -69,40 +117,7 @@ void	handle_pipe(t_cmd *c_table, int *pipefd);
 void	execute_command(t_cmd *current_cmd, t_shell *shell, t_cmd *head);
 void	check_for_path(t_cmd *cmd, t_shell *shell);
 void	cleaning_up(pid_t pid, int stdin, int stdout, t_shell *shell);
-
-//BUILTIN_UTILS
-int		handle_builtin(t_cmd *current_cmd, t_shell *shell);
-int		check_builtin(t_cmd *current_cmd);
-char	**env_remove_one(char **envp, char *str, int len);
-char	**env_add_new(char **envp, char *str);
-int		count_till_equal(char *str);
-
-//LEXER
-void	move_to_next_token(int	*position, char *input);
-int		lexing(t_lexer *lexer, t_process *process);
-int		find_quote_pair(t_lexer *lexer);
-int		add_token_node(t_lexer *lexer);
-int		get_single_tokens(t_lexer *lexer, char token);
-int		double_greater_and_smaler(t_lexer *lexer);
-int		tag_word(t_lexer *lexer);
-int		time_to_lex(t_lexer *lexer);
-void	skip_whitespace(t_lexer *lexer);
-void	free_struct_and_stop_lexing(t_lexer **lexer);
-t_data	*go_to_last_lexer_node(t_data *lexer);
-
-//COMMAND TABLE
-int		add_space_check(t_data *data);
-int		is_redirect(t_data *data);
-int		command_table(t_parser *parser, t_process *process);
-void	command_node_add_back(t_cmd **command, t_parser *parser);
-void	fill_command_struct(t_parser *parser);
-void	pipe_roules_check(t_parser *parser);
-void	redirect_roules_check(t_parser *parser);
-void	syntax_check(t_parser *parser);
-void	setup_parser_struct(t_parser *parser, t_cmd *command, t_lexer *lexer);
-void	syntax_error_print(t_data *data);
-void	heredoc_argument(t_data *data, t_cmd *command);
-void	repeat_set_data_next_save(t_data **data);
+void	clean_exit(int exit_code, t_shell *shell, t_cmd *cmd);
 
 //BUILTIN
 int		echo_builtin(char *str);
@@ -114,6 +129,13 @@ int		unset_builtin(char *str, t_shell *shell, int i);
 void	exit_builtin(t_cmd *cmd, t_shell *shell);
 int		handle_builtin_piped(t_cmd *current_cmd, t_shell *shell, t_cmd *head);
 int		count_len(char *str, int i);
+
+//BUILTIN_UTILS
+int		handle_builtin(t_cmd *current_cmd, t_shell *shell);
+int		check_builtin(t_cmd *current_cmd);
+char	**env_remove_one(char **envp, char *str, int len);
+char	**env_add_new(char **envp, char *str);
+int		count_till_equal(char *str);
 
 //SIGNALS
 void	catch_signals(t_coordinate *coordinate);
@@ -130,31 +152,10 @@ void	parser_free(t_parser **parser);
 void	shell_struct_free(t_shell **shell);
 void	free_command_struct(t_cmd **command);
 
-//SETUP
-void	first_setup(t_coordinate *coordinate, char **envp);
-void	setup_structs(t_coordinate *coordinate);
-
-//UTILS
-t_shell	**get_shell_struct(void);
-t_cmd	**get_command_struct(void);
-void	get_input(t_coordinate *coordinate);
+//HANDLE PROCESS
 void	stop_process(t_process *process);
-void	input_check(int ac, char **av, t_coordinate *coordinate);
 void	set_error_code(t_shell *shell, int error_code);
-void	expand(t_parser *s);
-int		expand_to_home(t_shell *shell, t_data *s);
 void	stop_loop(t_coordinate *coordinate);
 void	if_null_stop_process(char *string, t_parser *parser);
-void	update_cmd(t_data *s, char *var, int start, char *name);
-void	clean_exit(int exit_code, t_shell *shell, t_cmd *cmd);
-int		expander(t_shell *shell, t_data *s);
-void	handle_non_sign(t_data *s);
-void	free_and_do_again(char *var, char *vc, t_shell *shell, t_data *s);
-int		get_sign_location(char *str);
-char	*get_variable_to_expand(char *str, int sign_location);
-char	*search_for_var(char *var, char **envp, t_shell *shell);
-void	update_cmd(t_data *s, char *var, int start, char *name);
-char	*split_variable(char *str);
-int		get_len_export(char *str);
 
 #endif
